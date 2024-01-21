671ac11e81beb62e289da237b4514b104242e682
Alon Caspi <Aloncaspi3@gmail.com>
2024-01-21 16:54:22 +0200

    bug fix with the server.js

--- site/server.js
+++ site/server.js
@@ -6,7 +6,6 @@ const path = require('path');
 const app = express();
 const port = 3000;
 const fs = require('fs');
-const path = require('path');
 const rapidApiKeyPath = path.join(__dirname, 'key.txt');
 const rapidApiKey = fs.readFileSync(rapidApiKeyPath, 'utf8').trim();
 
